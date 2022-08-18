5fbf9ae3e661db6b32d48fd77a3a66b6369a14c5
Sepsad <sepehrsdp@gmail.com>
2022-08-18 20:02:55 +0430

    app.py is updated to handle new routing

--- app.py
+++ app.py
@@ -3,20 +3,45 @@ from importlib import import_module
 import os
 from flask import Flask, render_template, Response
 
+from pupil_apriltags import Detector
+
 # import camera driver
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
 
-# Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
+# this part has been commented and will be uncommented after testing
+# at_detector = Detector(
+#     families='tag36h11',
+#     nthreads=1,
+#     quad_decimate=2.0,
+#     quad_sigma=0.0,
+#     refine_edges=1,
+#     decode_sharpening=0.25,
+#     debug=0,
+# )
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
-    return render_template('index.html')
+    """Home Page."""
+    return render_template("index.html")
+
+
+@app.route('/world_view')
+def world():
+    """World view page"""
+    return render_template("world_view.html")
+
+
+@app.route('/mapped_view')
+def mapped():
+    """Mapped view page"""
+    return render_template("mapped.html")
 
 
 def gen(camera):
@@ -25,6 +50,14 @@ def gen(camera):
     while True:
         frame = camera.get_frame()
         yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+
+# this part has been commented and will be uncommented after testing
+# def gen_mapped(camera):
+#     """Video streaming generator function."""
+#     yield b'--frame\r\n'
+#     while True:
+#         frame = camera.get_frame()
+#         yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
 
 
 @app.route('/video_feed')
@@ -35,4 +68,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(threaded=True)
