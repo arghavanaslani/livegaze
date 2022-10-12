47a5fd5f407f145078a139d5505fd1b43fc261ef
arghavan <aslaniarghavan@gmail.com>
2022-10-12 14:26:03 +0330

    changes the output of frames() from .bytes to .jpg

--- camera_pupil.py
+++ camera_pupil.py
@@ -30,4 +30,4 @@ class Camera(BaseCamera):
             #start_time = time.time()
             frame, gaze = Camera.video_source.receive_matched_scene_video_frame_and_gaze()
             image = frame.bgr_pixels
-            yield (cv.imencode('.jpg', image)[1].tobytes(), gaze)
+            yield (image, gaze)
