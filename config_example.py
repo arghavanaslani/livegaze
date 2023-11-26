b50fd1b24da9f525e4efc62762168c205f608005
sina bolouki <sina.bolouki@gmail.com>
2023-11-26 07:58:13 +0100

    add config_example file

---
+++ config_example.py
@@ -0,0 +1,8 @@
+UPLOAD_PATH = '/static/artworks/'
+DB_TYPE = 'postgresql+psycopg2'
+DB_NAME = 'livegaze'
+DB_HOST = 'localhost'
+DB_USER = 'testUser'
+DB_PORT = 5432
+DB_PASSWORD = 'testPassword'
+SQLALCHEMY_DATABASE_URI = DB_TYPE+"://"+DB_USER+":"+DB_PASSWORD+"@"+DB_HOST+" "+str(DB_PORT)+"/"+DB_NAME
